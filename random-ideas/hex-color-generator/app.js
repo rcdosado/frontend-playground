f3a9fc9bd1d1d3c88b062006cb3cb0b5ca0dce25
roy <rcdosado@gmail.com>
2017-12-13 15:55:38 +0800

    center color value on screen

--- random-ideas/hex-color-generator/app.js
+++ random-ideas/hex-color-generator/app.js
@@ -1,6 +1,20 @@
 function getColor(){
-    return '#' + Math.random().toString(16);
+    return '#' + Math.random().toString(16).slice(2,8);
 
 }
 
-document.write(getColor());
+function setBackGround(){
+    var bgColor = getColor();
+
+    document.body.style.background = bgColor;
+    return bgColor;
+}
+
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+        var color = setBackGround();
+        var generateHere = document.getElementById("hex");
+        generateHere.innerHTML = '<h1> ' + color +'</h1>';
+        
+    }
+}
